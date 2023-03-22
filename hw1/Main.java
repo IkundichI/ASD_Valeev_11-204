58111ff6afb1cc065809504f3016eb9600620ec2
Valeev Artem <52968778+IkundichI@users.noreply.github.com>
2023-03-22 20:28:45 +0300

    add hw1

---
+++ hw1/Main.java
@@ -0,0 +1,33 @@
+package hw1;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] array = new int[n + 1];
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i;
+		}
+		while (scanner.hasNextInt()) {
+			int u = scanner.nextInt();
+			int v = scanner.nextInt();
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == u) {
+					array[i] = v;
+				}
+			}
+		}
+		System.out.println("Нет пути между вершинами: ");
+		for (int i = 1; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] != array[j]) {
+					System.out.println(i + " => " + j);
+				}
+			}
+		}
+	}
+}
